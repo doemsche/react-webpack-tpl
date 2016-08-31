48297346f071c4135e97d2e28bd5e7d19dd8ec3c
root <root@lnx-dos.inhouse.tweaklab.org>
2016-08-31 11:56:35 +0200

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin("Copyright Tweaklab.org @dos"),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tpl.html"
+    }),
+   new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true,
+    hot: true
+  } 
+}
+ 
